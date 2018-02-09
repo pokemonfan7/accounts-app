073fc353afb1e7a0ab75e0590fe68eac1c23e943
pokemonfan7 <rumblesoso7@gmail.com>
2018-02-09 11:47:58 +0800

    RecordForm

--- src/components/RecordForm.js
+++ src/components/RecordForm.js
@@ -1,19 +1,51 @@
 import React, { Component } from 'react';
 
 export default class RecordForm extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            date:"",
+            title:"",
+            amount:""
+        };
+        this.handle=this.handle.bind(this);
+        this.valid=this.valid.bind(this);
+    }
+
+    handle(event){
+        let name=event.target.name;
+        let value=event.target.value;
+        this.setState({
+        [name]:value
+        });
+    }
+
+    valid(){
+        return (this.state.date&&this.state.title&&this.state.amount);
+    }
+
     render() {
         return (
             <form action="" className="form-inline">
                 <div className="form-group">
-                    <input className="form-control" type="text" placeholder="Date" name="date" />
+                    <input className="form-control" type="text" placeholder="Date" name="date"
+                           value={this.state.date}
+                           onChange={this.handle}
+                    />
                 </div>
                 <div className="form-group">
-                    <input className="form-control" type="text" placeholder="Title" name="title" />
+                    <input className="form-control" type="text" placeholder="Title" name="title"
+                           value={this.state.title}
+                           onChange={this.handle}
+                    />
                 </div>
                 <div className="form-group">
-                    <input className="form-control" type="text" placeholder="Amount" name="amount" />
+                    <input className="form-control" type="text" placeholder="Amount" name="amount"
+                           value={this.state.amount}
+                           onChange={this.handle}
+                    />
                 </div>
-                <button className="btn btn-primary" type="submit">Create Record</button>
+                <button className="btn btn-primary" type="submit" disabled={!this.valid()}>Create Record</button>
             </form>
         );
     }
