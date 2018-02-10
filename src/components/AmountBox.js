fee4020c54e906116981871d52377f59c422f61d
pokemonfan7 <rumblesoso7@gmail.com>
2018-02-10 10:47:44 +0800

    box

--- src/components/AmountBox.js
+++ src/components/AmountBox.js
@@ -3,7 +3,7 @@ import React,{ Component } from 'react';
 export default class AmountBox extends Component{
     render(){
         return (
-            <div className="col">
+            <div className="col-md-3">
                 <div className="card">
                     <div className={`card-header bg-${this.props.type} text-white`}>
                         {this.props.title}
